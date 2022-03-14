064522ee3869356df1d95969aae4ef7804d706c8
haxhosajarber2021 <haxhosajarber2021@outlook.com>
2022-03-14 11:56:20 -0700

    Main updated

--- onlineshopapp/src/main.js
+++ onlineshopapp/src/main.js
@@ -1,15 +1,31 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BFormFile } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import { routes } from './routes'
 import router from './router'
-import vuetify from './plugins/vuetify'
+import store from './store'
+
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueRouter);
+Vue.component('b-form-file', BFormFile)
+
+// const router = new VueRouter({
+//     routes
+// });
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+    store,
+    router,
+    render: h => h(App),
 }).$mount('#app')
