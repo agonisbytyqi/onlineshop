d089b531c57519638f5c97b64b603f5825f850fe
haxhosajarber2021 <haxhosajarber2021@outlook.com>
2022-03-14 12:01:00 -0700

    Router and design implemented

--- onlineshopapp/src/router/index.js
+++ onlineshopapp/src/router/index.js
@@ -1,29 +1,85 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/components/Home.vue';
+import Header from '@/components/Header.vue';
+import About from '@/components/About.vue';
+import Footer from '@/components/Footer.vue';
+import AddUser from '@/components/users/AddUser.vue'
+import UsersList from '@/components/users/UsersList.vue'
+import ShowUsers from '@/components/users/ShowUsers.vue'
+import EditUser from '@/components/users/EditUser.vue'
+import Login from '@/components/auth/Login.vue'
+import Register from '@/components/auth/Register.vue'
+// import AddProducts from '@/components/AddProducts.vue';
+import Contact from '@/components/Contact.vue';
+import Products from '@/components/Products.vue'
+import AllProducts from '@/components/AllProducts.vue'
 
-Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+import VueRouter from 'vue-router';
+import firebase from '@/Firebase.js';
+import router from './index';
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
 
-export default router
+
+export default new VueRouter({
+    routes: [
+        { path: '/', name: 'Home', component: Home },
+        { path: '/header', name: 'Header', component: Header },
+        { path: '/about', name: 'About', component: About },
+        { path: '/footer', name: 'Footer', component: Footer },
+        {
+            path: '/users-list',
+            name: 'UsersList',
+            component: UsersList,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/show-user/:id',
+            name: 'ShowUsers',
+            component: ShowUsers,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/edit-user/:id',
+            name: 'EditUser',
+            component: EditUser,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/add-user',
+            name: 'AddUser',
+            component: AddUser,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        { path: '/login', name: 'Login', component: Login },
+        { path: '/register', name: 'Register', component: Register },
+        // { path: '/add-products', name: 'Products', component: AddProducts },
+        { path: '/contact', name: 'contact', component: Contact },
+        {
+            path: '/products',
+            name: 'Products',
+            component: Products,
+            meta: {
+                requiresAuth: true
+
+            }
+        },
+        { path: '/AllProducts', name: 'AllProducts', component: AllProducts },
+    ]
+});
+
+router.beforeEach(async(to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+    if (requiresAuth && !await firebase.getCurrentUser()) {
+        next('Login');
+    } else {
+        next();
+    }
+});
